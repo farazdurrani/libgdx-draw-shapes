95f55e8905c663d0ce2293d711ac1f4846276315
faraz <faraz.uic@gmail.com>
2025-10-15 08:07:53 -0500

    okay very nice. Got all the movements in bag. Now just need to finetune them and rearrange keys as to what each key does. That will be done later. For now... moving on to greener pastures.

--- core/src/main/java/com/faraz/libgdx/CustomFirstPersonCameraController.java
+++ core/src/main/java/com/faraz/libgdx/CustomFirstPersonCameraController.java
@@ -1,6 +1,5 @@
 package com.faraz.libgdx;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
 import com.badlogic.gdx.math.Vector3;
@@ -9,7 +8,10 @@ public class CustomFirstPersonCameraController extends FirstPersonCameraControll
 
     private static final float scrollFactor = -0.1f;
     private static final float translateUnits = 10f; // FIXME auto calculate this based on the target
+    private static final float rotSpeed = 0.2f;
     private final Vector3 tmpV1 = new Vector3();
+    private int mouseX = 0;
+    private int mouseY = 0;
 
     public CustomFirstPersonCameraController(Camera camera) {
         super(camera);
@@ -25,5 +27,37 @@ public class CustomFirstPersonCameraController extends FirstPersonCameraControll
         camera.translate(tmpV1.set(camera.direction).scl(amount * 50f));
         camera.update();
         return true;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        int magX = Math.abs(mouseX - screenX);
+        int magY = Math.abs(mouseY - screenY);
+
+        if (mouseX > screenX) {
+            camera.rotate(Vector3.Y, 1 * magX * rotSpeed);
+            camera.update();
+        }
+
+        if (mouseX < screenX) {
+            camera.rotate(Vector3.Y, -1 * magX * rotSpeed);
+            camera.update();
+        }
+
+        if (mouseY < screenY) {
+            if (camera.direction.y > -0.965) camera.rotate(camera.direction.cpy().crs(Vector3.Y), -1 * magY * rotSpeed);
+            camera.update();
+        }
+
+        if (mouseY > screenY) {
+
+            if (camera.direction.y < 0.965) camera.rotate(camera.direction.cpy().crs(Vector3.Y), 1 * magY * rotSpeed);
+            camera.update();
+        }
+
+        mouseX = screenX;
+        mouseY = screenY;
+
+        return false;
     }
 }
